71b23bd766b3d8fce218763351c03c570cca72a4
Elliott.P <124545912+elliottp123@users.noreply.github.com>
2025-01-25 10:19:57 +0000

    added the requirements for new security

--- main.py
+++ main.py
@@ -100,4 +100,5 @@ def view_entry(entry_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=False) 
+    #turn to True for logs
