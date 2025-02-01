1214e0fa9ea7ef177a5047bde557f2d91447d288
Elliott.P <124545912+elliottp123@users.noreply.github.com>
2025-02-01 23:37:33 +0000

    accessability lighthouse patches and style patch ( was on darkmode again >:/ )

--- reset_db.py
+++ reset_db.py
@@ -1,12 +1,12 @@
 import os
 
-folders_to_scan = ['template', 'static', '.']
+folders_to_scan = ['api', 'templates', 'static']
 
 for folder in folders_to_scan:
     if os.path.exists(folder):
-        for file in os.listdir(folder):
-            file_path = os.path.join(folder, file)
-            if os.path.isfile(file_path):
-                filename, file_extension = os.path.splitext(file)
-
-                print(f"@{filename}{file_extension}")
+        print(f"\n=== Files in {folder} folder ===")
+        for root, dirs, files in os.walk(folder):
+            for file in files:
+                file_path = os.path.join(root, file)
+                relative_path = os.path.relpath(file_path, folder)
+                print(f"@{relative_path}")
